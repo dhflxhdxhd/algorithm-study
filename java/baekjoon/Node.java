f6f0feefd17668a8cebcbaf997e863a2a948b4b5
or_hyn <dhflxhdxhd@gmail.com>
2024-08-23 20:36:47 +0900

    feat: 백준 1753.최단경로 풀이 완 다익스트라를 이용해서 한 정점부터 모든 정점에 대한 최단 경로비용을 구함

---
+++ java/baekjoon/Node.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+// Node 클래스: 정점과 해당 정점까지의 가중치를 나타냄
+class Node implements Comparable<Node>{
+    int vertex; // 정점
+    int weight; // 간선(가중치)
+
+    public Node(int vertex, int weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + vertex + " , " + weight + ")";
+    }
+
+    // 가중치 기준 오름차순 정렬
+    @Override
+    public int compareTo(Node other) {
+        return this.weight - other.weight;
+    }
+}
