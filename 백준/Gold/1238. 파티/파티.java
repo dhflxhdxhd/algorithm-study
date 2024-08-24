3eda16955ab24ae61612dda169c417d28f070b0a
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2024-08-24 18:06:57 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 18884 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+// Node 클래스: 정점과 해당 정점까지의 가중치를 나타냄
 class Node implements Comparable<Node>{
     int vertex; // 정점
     int weight; // 간선(가중치)
@@ -22,7 +23,10 @@ class Node implements Comparable<Node>{
     }
 }
 
-// 1238. 파티
+/*
+1238. 파티
+아이디어 : 역방향 그래프를 활용
+*/
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,8 +38,11 @@ public class Main {
         int x = Integer.parseInt(st.nextToken()); // 목적지
 
         List<List<Node>> adjList = new ArrayList<>(n+1);
+        List<List<Node>> reverseAdjList = new ArrayList<>(n+1);
+
         for (int i = 0; i <= n; i++) {
             adjList.add(new ArrayList<>());
+            reverseAdjList.add(new ArrayList<>());
         }
 
         for (int i = 1; i < m+1; i++) {
@@ -46,34 +53,28 @@ public class Main {
             int weight = Integer.parseInt(st.nextToken());
 
             adjList.get(startNode).add(new Node(endNode, weight));
+            reverseAdjList.get(endNode).add(new Node(startNode, weight)); // 역방향 그래프 생성
         }
 
-//        int[] distance = new int[n+1];
-//        Arrays.fill(distance, Integer.MAX_VALUE);
-        int[][] distance = new int[n+1][n+1];
-        for (int i = 0; i < n+1; i++) {
-            Arrays.fill(distance[i], Integer.MAX_VALUE);
-        }
+        int[] distanceFromX = new int[n+1]; // 목적지 x에서 집으로 돌아오는 최단경로
+        int[] distanceToX = new int[n+1]; // 모든 마을에서 목적지 x로 가는 최단 경로(역방향 그래프 활용)
+        Arrays.fill(distanceFromX, Integer.MAX_VALUE);
+        Arrays.fill(distanceToX, Integer.MAX_VALUE);
 
-        for (int i = 1; i < n+1 ; i++) {
-            dijkstra(adjList, distance[i], i); // 목적지에서 각 집으로 돌아가는 경우
-//            System.out.println(i+"번\t" + Arrays.toString(distance[i]));
-        }
+        dijkstra(adjList, distanceFromX, x);
+        dijkstra(reverseAdjList, distanceToX, x);
 
         int[] roundDistance = new int[n+1];
         Arrays.fill(roundDistance, 0);
         for (int i = 1; i < n+1; i++) {
-
-            // 목적지 마을이라면
-            if(i == x){
-                roundDistance[i] = 0;
-            }else{
-                roundDistance[i] = distance[i][x] + distance[x][i]; // x번 마을까지 가는 최소비용 + 다시 집으로 돌아오는 최소비용
+            if(i==x){
+                continue;
             }
+            roundDistance[i] = distanceFromX[i] + distanceToX[i];
         }
 
-        int maxTime = Arrays.stream(roundDistance).max().getAsInt();
-        System.out.println(maxTime);
+        int maxDistance = Arrays.stream(roundDistance).max().getAsInt();
+        System.out.println(maxDistance);
     }
 
     private static void dijkstra(List<List<Node>> adjList, int[] distance, int start){
