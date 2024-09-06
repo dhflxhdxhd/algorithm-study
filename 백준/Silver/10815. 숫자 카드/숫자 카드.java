0e893aff8258b24d1000fc6b1e68ba0bc67bddbf
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2024-09-06 16:35:44 +0900

    [Silver V] Title: 숫자 카드, Time: 832 ms, Memory: 139036 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,20 +5,20 @@ import java.util.*;
 public class Main {
 
     static int n,m;
-    static int[] sang, cards, result;
+    static int[]  cards, result;
+    static HashSet<Integer> hashSet;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
         n = Integer.parseInt(br.readLine());
-        sang = new int[n];
+        hashSet = new HashSet<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            sang[i] = Integer.parseInt(st.nextToken());
+            hashSet.add(Integer.parseInt(st.nextToken()));
         }
-
-        Arrays.sort(sang);
 
         m = Integer.parseInt(br.readLine());
         cards = new int[m];
@@ -33,39 +33,18 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            sb.append(result[i]+" ");
+            sb.append(result[i]).append(" ");
         }
 
         System.out.println(sb);
     }
 
+    /**
+     * 상근이가 카드를 가지고 있는지 확인하는 메소드
+     */
     private static void hasCards(){
         for (int i = 0; i < m; i++) {
-            int status = findCard(cards[i]);
-            result[i] = status;
+            result[i] = hashSet.contains(cards[i]) ? 1 : 0;
         }
-    }
-
-    private static int findCard(int card){
-        int result = 0;
-
-        // 이분탐색으로 상근이가 카드를 가지고 있는지 확인
-        int left = 0;
-        int right = n-1;
-
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-
-            if(sang[mid] < card){
-                left = mid + 1;
-            }else if(sang[mid] > card){
-                right = mid - 1;
-            }else{
-                result = 1;
-                return result;
-            }
-        }
-
-        return result;
     }
 }
