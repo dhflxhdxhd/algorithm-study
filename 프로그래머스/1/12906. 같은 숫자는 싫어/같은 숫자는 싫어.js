9a7dd9f891e5f2f756baa53f72dc2d56d2f7d138
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2024-10-02 19:03:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.65 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,14 +1,15 @@
 function solution(arr)
 {
-    var answer = [];
+    return arr.filter((value, index) => value != arr[index + 1]);
+//     var answer = [];
     
-    let temp = arr[0];
-    answer.push(temp);
-    for (let i = 1; i < arr.length; i++) {
-        if (temp != arr[i]) {
-            temp = arr[i];
-            answer.push(temp);
-        }
-    }
-    return answer;
+//     let temp = arr[0];
+//     answer.push(temp);
+//     for (let i = 1; i < arr.length; i++) {
+//         if (temp != arr[i]) {
+//             temp = arr[i];
+//             answer.push(temp);
+//         }
+//     }
+//     return answer;
 }
