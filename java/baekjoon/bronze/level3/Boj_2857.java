c146acdf255fd93942d079f1a0d8a4696233b66f
or_hyn <dhflxhdxhd@gmail.com>
2024-10-21 18:34:10 +0900

    백준 2857. FBI 정규표현식

---
+++ java/baekjoon/bronze/level3/Boj_2857.java
@@ -0,0 +1,42 @@
+package baekjoon.bronze.level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Boj_2857 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] members = new String[5];
+
+        for(int i=0; i<5; i++){
+            members[i] = br.readLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean check = false;
+        for(int i=0; i<5; i++){
+            if(isFBI(members[i])){
+                check = true;
+                sb.append(i+1).append(" ");
+            }
+        }
+
+        System.out.println(check ? sb.toString() : "HE GOT AWAY!");
+    }
+
+    private static boolean isFBI(String member){
+        String regex = ".*FBI.*";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(member);
+
+        while(matcher.find()){
+            return true;
+        }
+        return false;
+    }
+}
