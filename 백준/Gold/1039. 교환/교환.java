b97fa624735fe2b9b2c322118b321923fa8ac1a4
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2024-10-28 22:01:09 +0900

    [Gold II] Title: 교환, Time: 224 ms, Memory: 18968 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.java
+++ 백준/Gold/1039. 교환/교환.java
@@ -29,11 +29,9 @@ public class Main {
 
     private static void getMaxValue(int n, int k){
         Queue<CountNum> queue = new ArrayDeque<>();
-//        boolean[][] visited = new boolean[1000000+1][k+1];
         Set<String> visited = new HashSet<String>();
 
         queue.offer(new CountNum(n,0));
-//        visited[n][0] = true;
         visited.add(n+","+0);
 
         while(!queue.isEmpty()){
@@ -55,25 +53,29 @@ public class Main {
                         queue.offer(new CountNum(swapNumber, currentCount+1));
                         visited.add(nextSet);
                     }
-//                    if(swapNumber != 0 && !visited[swapNumber][currentCount+1]){
-//                        queue.offer(new CountNum(swapNumber, currentCount+1));
-//                        visited[swapNumber][currentCount+1] = true;
-//                    }
+
                 }
             }
         }
     }
 
-    private static int swapPlaces(int number, int idx1, int idx2){
-        char[] numberArr = String.valueOf(number).toCharArray();
-        char temp = numberArr[idx1];
-        numberArr[idx1] = numberArr[idx2];
-        numberArr[idx2] = temp;
 
-        if(numberArr[0] == '0'){
-            return 0;
-        }else{
-            return Integer.parseInt(new String(numberArr));
-        }
+    private static int swapPlaces(int number, int idx1, int idx2){
+        int length = (int) Math.log10(number) + 1;
+
+        int digit1 = (number / (int) Math.pow(10, idx1)) % 10;
+        int digit2 = (number / (int) Math.pow(10, idx2)) % 10;
+
+        if (digit1 == digit2) return number;
+
+        number -= digit1 * (int) Math.pow(10, idx1);  // idx1 자리를 0으로
+        number -= digit2 * (int) Math.pow(10, idx2);  // idx2 자리를 0으로
+
+        number += digit1 * (int) Math.pow(10, idx2);  // idx1 값을 idx2 자리로 이동
+        number += digit2 * (int) Math.pow(10, idx1);  // idx2 값을 idx1 자리로 이동
+
+        if (number / (int) Math.pow(10, length - 1) == 0) return 0;
+
+        return number;
     }
 }
