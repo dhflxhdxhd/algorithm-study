dbff0b4faa9130577d5b5234ace343987fb6654e
or_hyn <dhflxhdxhd@gmail.com>
2024-10-30 19:09:04 +0900

    백준 9342. 염색체 풀이 완

---
+++ java/baekjoon/silver/level3/Boj_9342.java
@@ -0,0 +1,35 @@
+package baekjoon.silver.level3;
+
+import java.util.*;
+import java.io.*;
+import java.util.regex.Pattern;
+
+// 9342. 염색체
+public class Boj_9342 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb =new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            String target = br.readLine();
+
+            String result = checkTarget(target) ? "Infected!" : "Good";
+
+            if(i == t-1){
+                sb.append(result);
+                continue;
+            }
+
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static boolean checkTarget(String target){
+        String pattern = "^[A-F]?A+F+C+[A-F]?$";
+
+        return Pattern.matches(pattern, target);
+    }
+}
