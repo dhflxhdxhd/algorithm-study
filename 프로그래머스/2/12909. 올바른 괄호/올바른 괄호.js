845f3644f8d47161db6665bf7aaabe508bdb0d83
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2024-11-04 16:17:22 +0900

    [level 2] Title: 올바른 괄호, Time: 29.01 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,27 +1,27 @@
 function solution(s){
     var answer = true;
     
-    let count = 0;
-    for(let i=0; i<s.length; i++){
-        let current = s[i];
-        
-        if(current === '('){
-            count++;
-        }else if(current === ')'){
-            count--;
-            
-            if(count < 0){
-                answer = false;
-                break;
+    let stack = [];
+    for(let word of s){
+        if(word === "("){
+            stack.push(word);
+        }else if(word === ")"){
+            if(stack[stack.length-1] == "("){
+                stack.pop();
+                continue;
             }
+            
+            answer = false;
+            break;
         }
     }
-
-    if(count > 0 || count < 0){
-        answer = false;
-    }else{
-        answer = true;
-    }
     
-    return answer;
+    if(answer){
+        if(stack.length > 0){
+            return false;
+        }
+        return answer;
+    }else{
+        return answer;
+    }
 }
