4924abc98c402d9cff487b1716d0c37c80145d75
or_hyn <dhflxhdxhd@gmail.com>
2024-11-20 17:50:06 +0900

    백준 2417. 정수 제곱근 풀이 완

---
+++ java/baekjoon/silver/level4/Boj_2417.java
@@ -0,0 +1,31 @@
+package baekjoon.silver.level4;
+
+import java.io.*;
+import java.util.*;
+
+// 2417. 정수 제곱근
+public class Boj_2417 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        long result = getSqrtNum(n);
+        System.out.println(result);
+    }
+
+    private static long getSqrtNum(long n) {
+        long left = 1;
+        long right = n;
+        long result = 0;
+        while(left <= right){
+            long mid = left + (right - left) / 2;
+            if(Math.pow(mid, 2) >= n){
+                result = mid;
+                right = mid-1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+}
