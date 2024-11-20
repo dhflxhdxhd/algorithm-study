e12f234052a0a8942ce2f16800415e123ec59918
or_hyn <dhflxhdxhd@gmail.com>
2024-11-20 16:41:39 +0900

    백준 1789. 수들의 합 풀이 완

--- java/baekjoon/silver/level5/Boj_1789.java
+++ java/baekjoon/silver/level5/Boj_1789.java
@@ -1,5 +1,28 @@
-package baekjoon.silver.level5;public class Boj_1789 {
-    public static void main(String[] args) {
-        // TODO: code application logic here
+package baekjoon.silver.level5;
+
+import java.io.*;
+import java.util.*;
+
+// 1789. 수들의 합
+public class Boj_1789 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long S = Long.parseLong(br.readLine());
+
+        int result = getCount(S);
+        System.out.println(result);
+    }
+
+    private static int getCount(long S) {
+        long currentSum = 0;
+        int currentNumber = 0;
+
+        while (currentSum <= S) {
+            currentNumber++;
+            currentSum += currentNumber;
+        }
+
+        return currentNumber - 1;
     }
 }
+
