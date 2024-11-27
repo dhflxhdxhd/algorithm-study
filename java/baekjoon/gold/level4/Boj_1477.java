3f28a62e2fcbd920ff06e4214ce39ab384817b42
or_hyn <dhflxhdxhd@gmail.com>
2024-11-27 20:11:03 +0900

    백준 1477. 휴게소 세우기 풀이 완 이진탐색 파라메트릭 서치

---
+++ java/baekjoon/gold/level4/Boj_1477.java
@@ -0,0 +1,63 @@
+package baekjoon.gold.level4;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_1477 {
+
+    static int n, m, l;
+    static int[] places;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 현재 휴게소의 개수
+        m = Integer.parseInt(st.nextToken()); // 더 지으려고 하는 휴게소의 개수
+        l = Integer.parseInt(st.nextToken()); // 고속도로 길이
+
+        st = new StringTokenizer(br.readLine());
+        places = new int[n+2];
+
+        places[0] = 0;
+        places[n+1] = l;
+        for (int i = 1; i <= n; i++) {
+            places[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(places);
+
+        int result = getMinDist();
+        System.out.println(result);
+    }
+
+    private static int getMinDist() {
+        int left = 1;
+        int right = l - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canBuild(mid) > m) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    private static int canBuild(int mid) {
+        int count = 0;
+
+        for (int i = 1; i <= n+1; i++) {
+            int temp = places[i] - places[i - 1];
+            count += temp / mid;
+
+            if( temp % mid == 0){
+                count--;
+            }
+        }
+
+        return count;
+    }
+}
