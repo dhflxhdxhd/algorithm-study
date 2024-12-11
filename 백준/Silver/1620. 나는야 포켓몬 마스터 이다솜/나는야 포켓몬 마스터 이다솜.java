3324e413c0a54a8499a2cc3a7fb1a818b9356009
or_hyn <52151533+dhflxhdxhd@users.noreply.github.com>
2024-12-11 16:41:18 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 476 ms, Memory: 50140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,6 +1,10 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 // 1620. 나는야 포켓몬 마스터 이다솜
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -10,35 +14,25 @@ public class Main {
         int n = Integer.parseInt(st.nextToken()); // 포켓몬의 개수
         int m = Integer.parseInt(st.nextToken()); // 맞춰야 할 문제 수
 
-        HashMap<Integer, String> dic = new HashMap<>();
-        HashMap<String, Integer> dic2 = new HashMap<>();
+        HashMap<String, String> dic = new HashMap<>();
+
         for (int i = 1; i <= n; i++) {
             String name = br.readLine();
-            dic.put(i, name);
-            dic2.put(name, i);
+            String number = String.valueOf(i);
+            dic.put(number, name);
+            dic.put(name, number);
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             String target = br.readLine();
-            if(isNumber(target)){
-                String value = dic.get(Integer.parseInt(target));
-                sb.append(value).append("\n");
-            }else{
-                Integer value = dic2.get(target);
-                sb.append(value).append("\n");
+            sb.append(dic.get(target));
+
+            if(i < m-1){
+                sb.append("\n");
             }
         }
 
-        System.out.println(sb.deleteCharAt(sb.length()-1).toString());
-    }
-
-    public static boolean isNumber(String input) {
-        try {
-            Double.parseDouble(input);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        System.out.println(sb.toString());
     }
 }
